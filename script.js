bfc8e44f136054e4cd60dabf5ac664ba63e910c7
Jonathon Nordquist <jonathon.nordquist@gmail.com>
2016-12-27 16:47:15 -0600

    Add behavior and styling to make closing X appear in search box

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 	$(".search-icon").on("click", function() {
 		$(this).addClass("hidden");
-		var $searchField = $(".wiki-search-field");
+		var $searchField = $("#wiki-search-field");
 		$searchField.removeClass("hidden");
 		requestAnimationFrame(function() {
 			$searchField.addClass("wide-search-field");
+			$(".x-cont").addClass("x-cont-move");
+			$(".bar-one").addClass("bar-one-move");
+			$(".bar-two").addClass("bar-two-move");
 		});
+		document.getElementById("wiki-search-field").focus();
 	})
 });
